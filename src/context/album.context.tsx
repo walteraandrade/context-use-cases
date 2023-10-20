0581c6766c7283aad900cd08d9f7fbc0210c17f8
walteraandrade <walteraandrade@gmail.com>
2023-10-20 06:22:23 -0300

    added album context and mocks

---
+++ src/context/album.context.tsx
@@ -0,0 +1,22 @@
+import * as React from "react"
+
+export interface Album {
+  folder: string
+  title: string
+  artits: string[]
+  year: number
+}
+
+const AlbumContext = React.createContext<{ album: Album } | null>(null)
+
+// eslint-disable-next-line react-refresh/only-export-components
+export function useAlbumContext() {
+  const context = React.useContext(AlbumContext)
+  if (!context) {
+    throw new Error("AlbumContext must be used inside AlbumCard")
+  }
+
+  return context
+}
+
+export default AlbumContext
